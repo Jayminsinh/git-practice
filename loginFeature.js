9b51119c6a08ad2725e866d6eaceee326f9ea201
Jayminsinh <jaymin.parmar@aliansoftware.com>
2026-03-02 14:46:03 +0530

    feat: Add all styles to the Login page - style page - style form-field and errors

--- loginFeature.js
+++ loginFeature.js
@@ -9,3 +9,16 @@
 
 
 // Add authantication logic for the Login users 
+
+
+
+
+// Add style
+
+// Add style to the form of login not errors
+
+// Add style to errors also
+
+// Add header
+
+// Style to header
